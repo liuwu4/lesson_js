4c1e7cb8e3700cf05dc6305b4d2b7d9f68ad8b3d
yj <liuwu4@yeah.net>
2021-03-19 10:44:45 +0800

    styles: 格式化

--- Test.js
+++ Test.js
@@ -1,7 +1,7 @@
-const { mysql } = require("./db/mysql");
+const { mysql } = require('./db/mysql');
 function test() {
-  mysql.query("select * from province", function (err, rows, fiels) {
-    console.log("====:", rows);
+  mysql.query('select * from province', function (err, rows, fiels) {
+    console.log('====:', rows);
   });
 }
 test();
